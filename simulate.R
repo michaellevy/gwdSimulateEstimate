787db5ce792f56d619bc1845c700eddb2d6ac358
Michael Levy <malevy@ucdavis.edu>
2016-10-10 19:10:21 -0700

    Simulate over mean degree and degpop-coef, estimate over theta_s, write csv

--- simulate.R
+++ simulate.R
@@ -3,75 +3,78 @@ if(!require("netUtils")) {
   devtools::install_github("michaellevy/netUtils")
 }
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(statnet, dplyr, ggplot2, netUtils, stargazer)
+pacman::p_load(statnet, tidyverse, netUtils, broom)
 set.seed(80112)
 
-n = makeNetwork(100, meanDegree = 1, directed = FALSE)    
-nSim = simulate.formula(
-  n ~ gwdegree(.1, TRUE)
-  , coef = -3
-  , constraints = ~ edges
-  , nsim = 1
-  , control = control.simulate(MCMC.burnin = 1e4, MCMC.interval = 1e4))
-decays = c(.1, .5, 1.5, 3)
-# 
-# ergms = lapply(decays, function(decay) 
-#   try(ergm(nSim ~ edges + gwdegree(decay, fixed = TRUE),
-#            control = control.ergm(MCMLE.maxit = 50,
-#                                   MCMC.samplesize = 5e3)))
-# )
+nNodes = 100
+meanDegs = 1:2 # c(1, 3, 10)
+coefs = c(-1, 1)  # seq(-.5, .5, len = 4)
+decays = c(.5, 2)  # 10^(seq(-1, .5, len = 5))  # May want better resolution here. 
 
-ergmsC = lapply(decays, function(decay) 
-  structure(
-    try(ergm(nSim ~ gwdegree(decay, fixed = TRUE),
-             control = control.ergm(MCMLE.maxit = 50,
-                                    MCMC.samplesize = 5e3),
-             constraints = ~ edges)),
-    "decay" = decay)
-)
+# Organized as models/generating_mechanism/constrained-or-density/fixed-or-CEF
+directory = "models/degPop/constrained/fixed"
+dir.create(directory, recursive = TRUE)
 
-# stargazer(ergms[sapply(ergms, class) == "ergm"], type = "text")
-ergmsC = ergmsC[sapply(ergmsC, class) == "ergm"]
-stargazer(ergmsC, type = "text")
-
-sims = lapply(ergmsC, function(model)
-  simulate.ergm(model, contstraints = ~ edges, nsim = 500))
-
-degDists = 
-  do.call(rbind,
-          lapply(seq_along(sims), function(i) {
-            deg = getDegreeDist(sims[[i]])
-            mutate(deg,
-                   decay = attr(ergmsC[[i]], 'decay'))
+estimates = 
+  lapply(meanDegs, function(meanDeg) {
+    
+    randomGraph = makeNetwork(nNodes, meanDegree = meanDeg, directed = FALSE)    
+    
+    coefWithinMeandeg = 
+      lapply(coefs, function(dp) {
+        
+        # Simulate a network with the given parameter value (dp) for this iteration
+        simNet = 
+          simulate.formula(
+            randomGraph ~ degreepopularity
+            , coef = dp
+            , constraints = ~ edges
+            , nsim = 1
+            # , control = control.simulate(MCMC.burnin = 1e4, MCMC.interval = 1e4))
+          )
+        
+        # lapply over theta_s values and estimate an ergm for each
+        decaysWithinCoef = 
+          lapply(decays, function(decay) {
+            
+            # Open connection to write warnings and note parameter values
+            lg = file(file.path(directory, "EstimationLog.txt"), open = "at")
+            sink(lg, type = "message", append = TRUE)
+            message("\nAt ", Sys.time(), ", with coef = ", dp, " & decay = ", decay)
+            
+            # Estimate
+            m = try(ergm(simNet ~ gwdegree(decay, fixed = TRUE),
+                         control = control.ergm(MCMLE.maxit = 50,
+                                                MCMC.samplesize = 5e3,
+                                                seed = 475)
+                         , constraints = ~ edges
+            ))
+            
+            # Close connection
+            sink(type = "message")
+            close(lg)
+            
+            if(class(m) == "try-error")  
+              return(NULL)
+            
+            tidy(m) %>%
+              mutate(theta_s = decay)
+            
           }) 
-  )
+        
+        if(all(sapply(decaysWithinCoef, is.null)))
+          return(NULL)
+        
+        do.call(rbind, decaysWithinCoef) %>%
+          mutate(degPop = dp)
+      })
+    
+    if(all(sapply(coefWithinMeandeg, is.null)))
+      return(NULL)
+    
+    do.call(rbind, coefWithinMeandeg) %>%
+      mutate(meanDegree = meanDeg)
+    
+  })
 
-# Calculate variance of degree distribution
-vars = lapply(seq_along(sims), function(i) {
-  c(decay = attr(ergmsC[[i]], "decay"), 
-    variance = mean(sapply(sims[[i]], function(n) var(degree(n, gmode = 'graph')))))
-}) %>% 
-  do.call(rbind, .) %>% 
-  as.data.frame()
-
-vars
-var(degree(nSim, gmode = "graph"))  # yikes
-
-png("results/varyDecayAndEst_gofs.png", width = 800, height = 1000)
-par(mfrow = c(4, 3))
-for(i in seq_along(ergmsC)) {
-  decay = attr(ergmsC[[i]], "decay")
-  plot(gof(ergmsC[[i]]))
-  mtext(decay)
-}
-dev.off()
-
-png("results/sampleGraphs.png", width = 800, height = 1000)
-par(mfrow = c(3, 2))
-plot(nSim, main = "basis")
-for(i in 1:length(sims))
-  plot(sims[[i]][[1]], main = paste0("Decay = ",
-                                     attr(ergmsC[[i]], "decay"),
-                                     "\nVariance = ",
-                                     round(vars$variance[i], 2)))
-dev.off()
+write_csv(do.call(rbind, estimates), file.path(directory, "estimates.csv"))
